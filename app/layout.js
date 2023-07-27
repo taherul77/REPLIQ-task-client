d7fde5ec7d5c1fd4352e971231411d237651c29d
taherul77 <taherul2317@gmail.com>
2023-07-27 15:58:16 +0600

    update nav

--- app/layout.js
+++ app/layout.js
@@ -1,12 +1,12 @@
 "use client";
-
+import Nav from "@/lib/components/Nav";
 import "./globals.css";
 import { Inter } from "next/font/google";
 import Footer from "@/lib/components/Footer";
 import { AuthProvider } from "@/lib/context/AuthProvider";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Toaster } from "react-hot-toast";
-import Nav from "@/lib/components/Nav";
+
 
 const inter = Inter({ subsets: ["latin"] });
 const queryClient = new QueryClient();
@@ -21,6 +21,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <div className="bg-white">
         <QueryClientProvider client={queryClient}>
           <Toaster position="top-center" reverseOrder={false} />
           <AuthProvider>
@@ -29,6 +30,7 @@ export default function RootLayout({ children }) {
             <Footer></Footer>
           </AuthProvider>
         </QueryClientProvider>
+        </div>
       </body>
     </html>
   );
